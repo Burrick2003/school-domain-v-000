459a6c24517c99874129f76b6808cf592814a067
Blakeley Bond <burrick2003@yahoo.com>
2017-11-08 03:01:02 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,12 @@
 class School
 @roster = {} #hash of grade hashes, each grade has array of names
-attr_accessor roster
+#attr_accessor roster
 
-def add_student
+def add_student(new_student) #adds [name, grade] to @roster
 
 
+end
+def grade (grade)#returns array of all students in that grade sorted alphabetically
 end
 
 end

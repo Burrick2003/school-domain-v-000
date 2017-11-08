5b831450f03bc1940ca6702953ebd902e2b36b12
Blakeley Bond <burrick2003@yahoo.com>
2017-11-08 03:40:37 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,11 +10,11 @@ attr_accessor :school_name, :roster
 
 def add_student(new_student) #adds [name, grade] to @roster
   if
-    @roster[new_student[1]] == nil
-    @roster[new_student[1]] = []
-    @roster = {new_student[1] => [new_student[0]]}
+    @roster[:new_student[1]] == nil
+    @roster[:new_student[1]] = []
+    @roster = {:new_student[1] => [new_student[0]]}
   else
-    @roster[new_student[1]] << new_student[0]
+    @roster[:new_student[1]] << new_student[0]
   end
 end
 def grade (grade)#returns array of all students in that grade sorted alphabetically

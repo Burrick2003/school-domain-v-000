88c88c36049bdbffef36055e6de28c8c933e8ce5
Blakeley Bond <burrick2003@yahoo.com>
2017-11-08 03:46:02 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,7 +13,7 @@ def add_student(new_name, grade) #adds [name, grade] to @roster
     roster[grade] = []
   end
   roster[grade] << new_name
-    
+
 end
 def grade (grade)#returns array of all students in that grade sorted alphabetically
 end
